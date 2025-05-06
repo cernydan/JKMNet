db5701fad32bfe4ae5ce17c50b04bbe8bdd44145
jakubcovam <michala.jakubcova@gmail.com>
2025-05-06 12:06:27 +0200

    Set MLP

---
+++ include/MLP.hpp
@@ -0,0 +1,15 @@
+class MLP {
+
+    public:
+    MLP();  //!< The constructor
+    ~MLP();  //!< The destructor     
+
+
+    // vytvorit objekt pro vahy, x, a, f(a)
+
+    protected:
+
+    private:
+
+
+}
